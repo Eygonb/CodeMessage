8396bd2c76928188b7dde8151691736e9e1e7f70
Eygonb <30298609+Eygonb@users.noreply.github.com>
2022-04-23 22:04:24 +0300

    Initial spring commit

---
+++ src/main/java/ru/vsu/tp/CodeMessage/CodeMessageApplication.java
@@ -0,0 +1,13 @@
+package ru.vsu.tp.CodeMessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeMessageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vsu/tp/CodeMessage/CodeMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.tp.CodeMessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
