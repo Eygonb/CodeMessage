ba9cb5ec847d60cdad24b58cf6824bc9b2c16757
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2022-04-28 21:54:51 +0300

    добавлена страница профиля

---
+++ src/messenger/Profile/Profile.js
@@ -0,0 +1,51 @@
+import {NavBar} from "../Navigation/NavBar";
+import * as React from 'react';
+import "./Profile.css"
+import "../../index.css"
+
+import profileIMG from '../assets/profile.png';
+
+export default function UserProfile() {
+
+    return (
+        <div >
+            <NavBar/>
+            <div className="profile-box">
+                {/*<span className="text-center">Profile</span>*/}
+
+                <div className="profile-header">
+                    <div className="profile-header-item">
+
+                        <img src={profileIMG} className="profile-header-item"/>
+                        <span className="profile-header-item text-center1">NAme</span>
+                    </div>
+                </div>
+
+                <div className="profile-element">
+                    <span className="profile-header-item text1">Some User Description</span>
+                    <p className="text-description">
+                        About User
+                    </p>
+                    <span className="profile-header-item text1">@username</span>
+                    <p className="text-description">
+                        username
+                    </p>
+                </div>
+
+                <div className="profile-element profile-header-item">
+                    <button type="button" className="btn-send">
+                        <span className="text1">Send Message</span>
+                    </button>
+                </div>
+
+                <div className="profile-element profile-header-item">
+                    <button type="button"  className="btn-send" style={{backgroundColor: "#00793CFF"}} >
+                        <span className="text1">Edit Profile</span>
+                    </button>
+                </div>
+
+
+            </div>
+        </div>
+    );
+}
