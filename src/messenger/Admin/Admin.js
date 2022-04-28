5d4ace75d028ef651dc11f3a38db24709e37d3fe
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2022-04-28 21:54:06 +0300

    добавлена админ панель

---
+++ src/messenger/Admin/Admin.js
@@ -0,0 +1,33 @@
+import {NavBar} from "../Navigation/NavBar";
+import * as React from 'react';
+import "./Admin.css"
+import "../../index.css"
+import ConversationSearch from "../ConversationSearch";
+
+
+export default function Admin() {
+
+    return (
+        <div>
+            <NavBar/>
+                <div className="admin-box">
+                    <span className="textA">admin page</span>
+                    <div className="search-box">
+                        <ConversationSearch/>
+                    </div>
+                    <div className="admin-page-element">
+                        <button type="button" className="btn-admin">
+                            users list
+                        </button>
+                    </div>
+                    <div className="admin-page-element" >
+                        <button type="button" className="btn-admin" style={{backgroundColor: "#006DC0FF"}}>
+                            reports list
+                        </button>
+                    </div>
+
+                </div>
+
+        </div>
+    );
+}
