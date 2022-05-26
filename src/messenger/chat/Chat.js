d75fa6647840a191315de786618ba898dd4d5ec4
Данияр Иркагалиев <80203074+DaniyarIrkagaliev@users.noreply.github.com>
2022-05-26 20:33:47 +0300

    слега переделан чат и изменен дизайн

---
+++ src/messenger/chat/Chat.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+import Conversation from "./Conversation/Conversation";
+import ConversationList from "./ConversationList/ConversationList";
+import axios from "axios";
+import ConversationListItem from "./ConversationList/ConversationListItem";
+
+
+export default function Chat1() {
+
+}
---
+++ src/messenger/chat/ChatMenu.js
@@ -0,0 +1,43 @@
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
+import ConversationList from "./ConversationList/ConversationList";
+import Conversation from "./Conversation/Conversation";
+
+export default function ChatMenu() {
+
+    const [user, setConversations] = useState([]);
+    useEffect(() => {
+        getConversations()
+    }, [])
+
+    const getConversations = () => {
+        axios.get('https://randomuser.me/api/?results=1').then(response => {
+            let newConversations = response.data.results.map(result => {
+                return {
+                    photo: result.picture.large,
+                    name: `${result.name.first} ${result.name.last}`,
+                    mes_text: `${result.gender}`,
+                    unread: `${Math.floor(Math.random() * 8)}`
+                };
+            });
+            setConversations([...user, ...newConversations])
+        });
+    }
+
+
+    return (
+        <div className='app1'>
+            <ConversationList/>
+            {
+                user.map(conversation =>
+
+                    <Conversation
+                        key={conversation.name}
+                        data={conversation}
+                    />
+                )
+            }
+
+        </div>
+    );
+}
