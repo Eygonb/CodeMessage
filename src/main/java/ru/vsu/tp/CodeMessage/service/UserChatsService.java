572aa1e70a285be392d19d9d8d106a056d099d96
Eygonb <30298609+Eygonb@users.noreply.github.com>
2022-06-06 17:34:11 +0300

    Fix singletons

--- src/main/java/ru/vsu/tp/CodeMessage/service/UserChatsService.java
+++ src/main/java/ru/vsu/tp/CodeMessage/service/UserChatsService.java
@@ -1,6 +1,5 @@
 package ru.vsu.tp.CodeMessage.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.vsu.tp.CodeMessage.entity.UserChat;
 import ru.vsu.tp.CodeMessage.entity.id.UserChatsId;
